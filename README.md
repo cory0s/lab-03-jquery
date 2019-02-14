941c064a9dceac5b6d009cb20ff6e30b93aecd5c
Cory Henderson <chenderson19899@gmail.com>
2019-02-14 10:21:58 -0800

    adds feature 1

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # lab-03-jquery
 Contains files for jQuery selectors and events lab
+
+Number and name of feature: Display Images
+
+Estimate of time needed to complete: 2hrs
+
+Start time: 9:40am
+
+Finish time: _____
+
+Actual time needed to complete: _____
+
+Driver: Cory
+Navigator: Jon
---
+++ js/app.js
@@ -0,0 +1,42 @@
+'use strict';
+
+function Creature(creature){
+  this.name = creature.title;
+  this.image_url = creature.image_url;
+  this.description = creature.description;
+  this.keyword = creature.keyword;
+  this.horns = creature.horns;
+}
+
+Creature.allCreatures = [];
+
+Creature.prototype.render = function() {
+  $('main').append('<div class="clone"></div>');
+
+  let creatureClone = $('div[class="clone"]');
+  let creatureHtml = $('#photo-template').html();
+
+  creatureClone.html(creatureHtml);
+
+  creatureClone.find('h2').text(this.name);
+  creatureClone.find('img').attr('src', this.image_url);
+  creatureClone.find('p').text(this.description);
+
+}
+
+Creature.readJson = () => {
+  console.log('ran readJson');
+  $.get('../data/page-1.json', 'json')
+    .then(data => {
+      data.forEach(obj => {
+        Creature.allCreatures.push(new Creature(obj));
+      })
+    })
+    .then(Creature.loadCreatures)
+}
+
+Creature.loadCreatures = () => {
+  Creature.allCreatures.forEach(creature => creature.render())
+}
+
+$(() => Creature.readJson());
